8638cab2ee8a4c552f124de9577baed640225cfa
Guillaume De Backere <guillaume.debackere@octoo.be>
2024-06-18 13:33:41 +0200

    feat:start making routes with vue router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { h } from 'vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      // Dynamic import -> used to make the page lazy loaded
+      component: () => import('@/views/HomeView.vue'),
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      // Dynamic import -> used to make the page lazy loaded
+      component: () => import('@/views/ProjectsView.vue'),
+    },
+    {
+      path: '/projects/:id',
+      name: 'single-project',
+      component: () => import('@/views/SingleProjectView.vue'),
+    },
+    {
+      // Catch all unknown routes in Vue router and redirect to a not found page
+      path: '/:catchAll(.*)*',
+      name: 'notFound',
+      component: h('p', { style: 'color:red;' }, '404 Not Found'),
+    },
+  ],
 })
 
 export default router
