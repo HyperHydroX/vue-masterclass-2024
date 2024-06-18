87ac9c1733c46f6095a4cab9e9f235b78deb1b2d
Guillaume De Backere <guillaume.debackere@octoo.be>
2024-06-18 16:22:22 +0200

    feat:seeded the db with fakerjs, node & supabase cli

--- db/seed.js
+++ db/seed.js
@@ -1,6 +1,23 @@
+import { createClient } from '@supabase/supabase-js'
 import { fakerEN_GB as faker } from '@faker-js/faker'
 
-const personName = faker.person.fullName()
-const personBio = faker.person.bio()
+const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY)
 
-console.log('Hi! My name is', personName, '. I am a', personBio)
+async function seedProjects(numEntries = 10) {
+  const projects = []
+
+  for (let i = 0; i < numEntries; i++) {
+    const name = faker.lorem.words(3)
+
+    projects.push({
+      name,
+      slug: name.toLocaleLowerCase().replace(/ /g, '-'),
+      status: faker.helpers.arrayElement(['in-progress', 'completed']),
+      collaborators: faker.helpers.arrayElements([1, 2, 3]),
+    })
+  }
+
+  await supabase.from('projects').insert(projects)
+}
+
+await seedProjects()
